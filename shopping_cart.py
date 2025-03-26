551defd06d3c2f5f736d9f52608dae7b7ef86885
Lakshan Jayalath <lakshanjayalath171@gmail.com>
2025-03-26 19:20:00 +0530

    Add files via upload
    *I created shopping cart program for using python basics.
    *I created encryption program using python strings.

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,3 +2,24 @@ foods = []
 pricess = []
 
 total = 0
+
+while True:
+
+    choose = input("Enter A Name You Want Buy ('Q' for quit): ")
+    upper_choose = choose.upper()
+    if(upper_choose=="Q" ):
+        break
+    else:
+        price = float(input(f"Enter A Price Of {choose} $"))
+        foods.append(choose)
+        pricess.append(price)
+
+print("-----YOUR CART------")
+
+for food in foods:
+    print(food, end=" ")
+
+for price in  pricess:
+    total +=price
+
+print(f"Your Total is {total}")
